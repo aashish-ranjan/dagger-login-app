d96d05e71eaa1b95477460185abc9ff44ea44b85
aashish-ranjan <aashishranjan2011@gmail.com>
2022-05-18 02:38:16 +0530

    provide glide dependency to activity

--- app/src/main/java/com/aashish/daggerloginapp/AuthActivity.java
+++ app/src/main/java/com/aashish/daggerloginapp/AuthActivity.java
@@ -1,10 +1,26 @@
 package com.aashish.daggerloginapp;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class AuthActivity extends DaggerAppCompatActivity {
+
+    @Inject
+    Drawable logo;
+
+    @Inject
+    RequestOptions requestOptions;
+
+    @Inject
+    RequestManager requestManager;
 
     private static final String TAG = "AuthActivity";
 
@@ -12,5 +28,10 @@ public class AuthActivity extends DaggerAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        requestManager
+                .setDefaultRequestOptions(requestOptions)
+                .load(logo)
+                .into((ImageView) findViewById(R.id.login_logo));
     }
 }
--- app/src/main/java/com/aashish/daggerloginapp/di/AppModule.java
+++ app/src/main/java/com/aashish/daggerloginapp/di/AppModule.java
@@ -1,7 +1,36 @@
 package com.aashish.daggerloginapp.di;
 
+import android.app.Application;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
+import com.aashish.daggerloginapp.R;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class AppModule {
+
+    @Provides
+    static RequestOptions provideRequestOptions() {
+        return new RequestOptions()
+                .error(R.drawable.white_background)
+                .placeholder(R.drawable.white_background);
+    }
+
+    @Provides
+    static Drawable providesLogo(Application application) {
+        return ContextCompat.getDrawable(application, R.drawable.logo);
+    }
+
+    @Provides
+    static RequestManager provideGlideInstance(Application application) {
+        return Glide.with(application);
+    }
+
 }
