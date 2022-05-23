87bb7fcb1cc1c6fe76902515e67038d6847baf95
aashish-ranjan <aashishranjan2011@gmail.com>
2022-05-23 12:30:51 +0530

    remove explicit type argument in AuthResource obj creation

--- app/src/main/java/com/aashish/daggerloginapp/models/AuthResource.java
+++ app/src/main/java/com/aashish/daggerloginapp/models/AuthResource.java
@@ -15,15 +15,15 @@ public class AuthResource<T> {
     }
 
     public static <T> AuthResource<T> success(@Nullable T data) {
-        return new AuthResource<T>(data, null, Status.AUTHENTICATED);
+        return new AuthResource<>(data, null, Status.AUTHENTICATED);
     }
 
     public static <T> AuthResource<T> error(@Nullable T data, @Nullable String message) {
-        return new AuthResource<T>(data, message, Status.ERROR);
+        return new AuthResource<>(data, message, Status.ERROR);
     }
 
     public static <T> AuthResource<T> loading(@Nullable T data) {
-        return new AuthResource(data, null, Status.LOADING);
+        return new AuthResource<>(data, null, Status.LOADING);
     }
 
     public static <T> AuthResource<T> logout() {
